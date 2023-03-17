9f2955b9bcf1121f144ebb8b9c6c709f401f43d6
iamclement1 <blessedmarcel1@gmail.com>
2023-03-17 11:09:14 +0100

    feats: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className="text-4xl font-bold">
+        Welcome to DYD
+      </h1>
+    </div>
+  )
+}
+
+export default App
