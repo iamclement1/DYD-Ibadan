6078bf5e9df73f3d50397a371db7fe41c83fbe82
iamclement1 <blessedmarcel1@gmail.com>
2023-03-17 20:09:13 +0100

    'feats: created navbar component and dashboard'

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+// import { Link } from "react-router-dom";
+import { useState } from "react";
+// import { FaBars } from 'react-icons/fa'
+
+const Navbar : React.FC = () => {
+    const [open, setOpen] = useState<boolean>(false);
+    return (
+        <section>
+            <h1>welcome</h1>
+        </section>
+    )
+}
+
+export default Navbar
