ac6e2803c25ac6d8ab3cdb64d86e30c2899b1091
iamclement1 <blessedmarcel1@gmail.com>
2023-03-19 13:59:50 +0100

    set hero-session background

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        heroBgColor: "rgba(0,0,0,0.5)",
+      }
+    },
   },
   plugins: [],
 }
