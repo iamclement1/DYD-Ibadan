1729834aec40e71bbd0d9ec9fad0cd7df4eb9f49
iamclement1 <blessedmarcel1@gmail.com>
2023-03-19 17:53:49 +0100

    configured the screen size

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,9 +6,18 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors : {
+      colors: {
         heroBgColor: "rgba(0,0,0,0.5)",
-      }
+      },
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+        "2xl": '1536px',
+        "3xl": '1850',
+        "4xl": '2400',
+      },
     },
   },
   plugins: [],
