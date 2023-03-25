6e51419b292fae10b9208d8c1815ad28a7494a02
iamclement1 <blessedmarcel1@gmail.com>
2023-03-25 20:01:23 +0100

    imported components as default

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,6 @@ export { default as Loader } from './Loader';
 export { default as Modal } from './Modal';
 export { default as Hero } from './Hero';
 export { default as Newsletter } from './Newsletter';
+export { default as Executives} from './OurExecutives';
+export { default as PrayerForm } from './PrayerForm';
 export { default as Footer } from './Footer';
